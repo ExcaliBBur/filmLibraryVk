82c0e6bac11c691f14ab68ce165fc374cd6cf61a
Alexander <excalibburn@shinra-tensei.ru>
2024-03-18 20:41:45 +0300

    add docker-compose

--- internal/model/entity/actor.go
+++ internal/model/entity/actor.go
@@ -3,8 +3,8 @@ package entity
 import "time"
 
 type Actor struct {
-	Id       int       `json:"id""`
-	Name     string    `json:"name""`
+	Id       int       `json:"id"`
+	Name     string    `json:"name"`
 	Sex      string    `json:"sex"`
 	Birthday time.Time `json:"birthday" time:"2006-01-02"`
 }
